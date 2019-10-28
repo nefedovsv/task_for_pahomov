a1b5f813cd3a1f1371af8003f1e89d164a2a3126
nefedovsv <47773817+nefedovsv@users.noreply.github.com>
2019-10-28 10:51:16 +0300

    убрал лишний логгер

--- src/components/RouteContainer/PrivateRoute.tsx
+++ src/components/RouteContainer/PrivateRoute.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Redirect, RouteProps } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { IStore } from "../../interfaces";
-import { any } from "prop-types";
 
 export const PrivateRoute = ({ component, ...rest }: RouteProps) => {
   const isAuthenticated = useSelector((store: IStore) => store.userData);
